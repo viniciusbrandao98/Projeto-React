93a11a18f792cc2af61a7cf7d8b575a3aa813a19
Vinícius Brandão <viniciusbrandao9417@gmail.com>
2026-07-24 18:30:36 -0300

    feat: layouts iniciais ajustados para da inicio ao projeto propriamente dito.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import Company from './components/pages/Company';
+import NewProject from './components/pages/NewProject';
+import Container from './components/layout/Container';
+
 function App() {
   return (
-    <div className="App">
-      <p>Projeto React</p>
-    </div>
+    <Router>
+      <div>
+        <Link to='/'>Home</Link>
+        <Link to='/company'>Empresa</Link>
+        <Link to='/contact'>Contato</Link>
+        <Link to='/newproject'>Novo Projeto</Link>
+      </div>
+      <Container customClass="min-height">
+      <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/newproject" element={<NewProject />} />
+      </Routes>
+      </Container>
+    </Router>
   );
 }
 
